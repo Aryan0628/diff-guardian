dbaad83f4e892e12ece1dd3894f4823bd6e6b905
Aryangupta <aryan072806@gmail.com>
2026-04-15 02:40:27 +0530

    feat: add R08 rule to detect breaking changes from exported to unexported functions

--- src/classifier/rules/index.ts
+++ src/classifier/rules/index.ts
@@ -5,3 +5,4 @@ export * from './R04_param_type_narrowed';
 export * from './R05_optional_param_added';
 export * from './R06_return_nullable';
 export * from './R07_return_narrowed';
+export * from './R08_unexported';
