05a01c13210f58ccd80fb7e8431a774c7471470d
Aryangupta <aryan072806@gmail.com>
2026-04-15 19:09:13 +0530

    feat: add R25 rule to flag newly required interface properties

--- src/classifier/rules/index.ts
+++ src/classifier/rules/index.ts
@@ -18,4 +18,5 @@ export * from './R21_async_to_sync';
 export * from './R22_return_never';
 export * from './R23_default_value_changed';
 export * from './R24_constructor_changed';
+export * from './R25_interface_property_required';
 
